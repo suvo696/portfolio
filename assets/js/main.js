657af16a6a8bd120ebbb9315a65fa4cd02cfee52
suvo696 <reachshuvojit@gmail.com>
2022-09-10 13:03:00 +0530

    ---

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,6 @@ function topFunction() {
 let header = $(`
 <nav class="fixed-top black">
 <div class="nav-wrapper">
-    <a href="#!" class="brand-logo"><img src="assets/images/John-Doe1.png"></a>
     <a href="#" data-target="mobile-demo" class="white-text sidenav-trigger"><i
             class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
